0e44362fc8cdf5e57e964440cdd345e4c92621f6
S3BA54nton <144800860+S3BA54nton@users.noreply.github.com>
2024-08-03 16:34:50 +0800

    Update app.js

--- app.js
+++ app.js
@@ -495,5 +495,5 @@ process.on('SIGQUIT', finalClose);
 // Start the server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
